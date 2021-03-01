c034d37e0806f2049af61d2304aad156a889146e
LarsVomMars <developer@kroenner.eu>
2021-03-01 14:54:42 +0100

    Small fixibus

--- slstatus/headsetcontrol.c
+++ slstatus/headsetcontrol.c
@@ -1,47 +1,52 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_BATTERY 16
 
 static short ctr = 0;
-static char prev[1024];
-static const short MAX_BATTERY = 16;
+static char prev[32] = {0};
 
 void append(char *string, char c) {
-    size_t len = strlen(string);
-    string[len] = c;
-    string[len + 1] = '\0';
+  size_t len = strlen(string);
+  string[len] = c;
+  string[len + 1] = '\0';
 }
 
 char *ret(char *string) {
-    strcpy(prev, string);
-    return string;
+  strcpy(prev, string);
+  return prev;
 }
 
 char *headsetcontrol() {
-    if (++ctr % 60 != 0) return prev;
-    ctr = 0;
-    FILE *file;
-    char c;
-    char result[1024];
-    file = popen("headsetcontrol -b -c", "r");
-    if (file == NULL) return "";
-    while ((c = fgetc(file)) != EOF) append(result, c);
+  if (++ctr % 60 != 0)
+    return prev;
+  ctr = 0;
+  FILE *file;
+  char c;
+  char result[8] = {0};
+  file = popen("headsetcontrol -b -c", "r");
 
-    if (strlen(result) == 0) ret("Off");
+  if (file == NULL)
+    return NULL;
 
-    // Seems to work
-    char tres[strlen(result)];
-    strcpy(tres, result);
+  while ((c = fgetc(file)) != EOF)
+    append(result, c);
 
-    long battery = strtol(tres, NULL, 10);
+  if (strlen(result) == 0)
+    ret("Off");
 
-    if (battery == -2) return ret("Off");
-    else if (battery == -1) return ret("Charging");
-    else if (0 <= battery && battery <= 100) {
-        double hours = (double) battery * (MAX_BATTERY / 100.0);
-        char value[1024];
-        sprintf(value, "%ld%% (~%.2fh)", battery, hours);
-        return ret(value);
-    } else if (battery == ']') return "sadge";
-    else return ret("idk m8");
+  long battery = strtol(result, NULL, 10);
+
+  if (battery == -2)
+    return ret("Off");
+  else if (battery == -1)
+    return ret("Charging");
+  else if (battery >= 0 && battery <= 100) {
+    double hours = (double)battery * (MAX_BATTERY / 100.0);
+    char value[32] = {0};
+    sprintf(value, "%ld%% (~%.2fh)", battery, hours);
+    return ret(value);
+  } else
+    return ret("idk m8");
 }
